185d47c9c149b060d3bfb6988ddb78946c7d0b7e
kracunovic92 <lazar.kracunovic92@gmail.com>
2025-01-18 15:21:24 +0100

    [add] Basic Expression parsing, LiteralNode

--- src/main/java/ASTBuilder.java
+++ src/main/java/ASTBuilder.java
@@ -3,6 +3,7 @@ import com.ibm.icu.message2.Mf2DataModel;
 import org.antlr.v4.runtime.tree.ParseTree;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 public class ASTBuilder extends CPP14ParserBaseVisitor<ASTNode>{
     @Override
@@ -151,6 +152,11 @@ public class ASTBuilder extends CPP14ParserBaseVisitor<ASTNode>{
             visitDeclarationStatement(ctx.declarationStatement(), variableDeclaration);
             return variableDeclaration;
         }
+        if (ctx.expressionStatement() != null) {
+            CPP14Parser.ExpressionStatementContext expression = ctx.expressionStatement();
+            CPP14Parser.ExpressionContext expr = expression.expression();
+            return visitExpression(expr);
+        }
         // TODO add other statements
         return null;
     }
@@ -186,6 +192,30 @@ public class ASTBuilder extends CPP14ParserBaseVisitor<ASTNode>{
             }
         }
     }
+    public ASTNode visitInitializer(CPP14Parser.InitializerContext ctx) {
+        if(ctx.braceOrEqualInitializer() != null) {
+            return visitInitializerClause(ctx.braceOrEqualInitializer().initializerClause());
+        }
+        return null;
+    }
+
+    public ASTNode visitInitializerClause(CPP14Parser.InitializerClauseContext ctx) {
+        if(ctx.assignmentExpression() != null) {
+            return visitAssignmentExpression(ctx.assignmentExpression());
+        }else if(ctx.bracedInitList() != null) {
+            return visitInitializerList(ctx.bracedInitList().initializerList());
+        }
+        return null;
+    }
+    public ASTNode visitInitializerList(CPP14Parser.InitializerListContext ctx) {
+        ExpressionNode exp = new ExpressionNode();
+        List<CPP14Parser.InitializerClauseContext> list = ctx.initializerClause();
+
+        for(CPP14Parser.InitializerClauseContext elem : list){
+            exp.addChild(visitInitializerClause(elem));
+        }
+        return exp;
+    }
 
     private void visitDeclSpecifierSeq(CPP14Parser.DeclSpecifierSeqContext ctx, VariableDeclarationNode variable) {
 
@@ -193,5 +223,210 @@ public class ASTBuilder extends CPP14ParserBaseVisitor<ASTNode>{
         //DeclaratorTypeNode node = DeclaratorTypeNode();
         //visitDeclSpecifier(ctx.declSpecifier(0), variable);
         variable.setType(ctx.declSpecifier(0).getText());
+    }
+
+    public ASTNode visitExpression(CPP14Parser.ExpressionContext ctx) {
+        //TODO check if this should change
+        return visitAssignmentExpression(ctx.assignmentExpression(0));
+    }
+
+    public ASTNode visitAssignmentExpression(CPP14Parser.AssignmentExpressionContext ctx) {
+
+        if(ctx.conditionalExpression() != null) {
+            return visitConditionalExpression(ctx.conditionalExpression());
+        }else{
+            ExpressionNode left = (ExpressionNode) visitLogicalOrExpression(ctx.logicalOrExpression());
+            String operator = ctx.assignmentOperator().getText();
+            ExpressionNode right = (ExpressionNode) visitLogicalOrExpression(ctx.logicalOrExpression());
+        }
+        return null;
+    }
+    public ASTNode visitConditionalExpression(CPP14Parser.ConditionalExpressionContext ctx) {
+
+        ExpressionNode exp = new ExpressionNode();
+        if(ctx.logicalOrExpression() != null) {
+            visitLogicalOrExpression(ctx.logicalOrExpression(), exp);
+            return exp;
+        }else{
+            return visitAssignmentExpression(ctx.assignmentExpression());
+        }
+    }
+
+
+
+    private <T> void visitExpression_template(
+            List<T> list,
+            String type,
+            String value,
+            ExpressionNode expression,
+            BiConsumer<T, ExpressionNode> visitor
+    ){
+        /*
+            Template for going through Expressions
+         */
+        if (list.size() > 1){
+            expression.setType(type);
+            expression.setValue(value);
+            for(T item : list){
+                ExpressionNode tmp = new ExpressionNode();
+                visitor.accept(item, tmp);
+                expression.addChild(tmp);
+            }
+        }else{
+            visitor.accept(list.getFirst(), expression);
+        }
+    }
+
+    private void visitLogicalOrExpression(CPP14Parser.LogicalOrExpressionContext ctx, ExpressionNode expression) {
+
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.logicalAndExpression(),
+                "LogicalOrExpression",
+                value,
+                expression,
+                this::visitLogicalAndExpression
+        );
+    }
+    private void visitLogicalAndExpression(CPP14Parser.LogicalAndExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.inclusiveOrExpression(),
+                "LogicalAndExpression",
+                value,
+                expression,
+                this::visitInclusiveOrExpression
+        );
+    }
+    private void visitInclusiveOrExpression(CPP14Parser.InclusiveOrExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.exclusiveOrExpression(),
+                "InclusiveOrExpression",
+                value,
+                expression,
+                this::visitExclusiveOrExpression
+        );
+    }
+    private void visitExclusiveOrExpression(CPP14Parser.ExclusiveOrExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.andExpression(),
+                "ExclusiveOrExpression",
+                value,
+                expression,
+                this::visitAndExpression
+        );
+    }
+    private void visitAndExpression(CPP14Parser.AndExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.equalityExpression(),
+                "AndExpression",
+                value,
+                expression,
+                this::visitEqualityExpression
+        );
+    }
+
+    private void visitEqualityExpression(CPP14Parser.EqualityExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.relationalExpression(),
+                "EqualityExpression",
+                value,
+                expression,
+                this::visitRelationalExpression
+        );
+    }
+    private void visitRelationalExpression(CPP14Parser.RelationalExpressionContext ctx, ExpressionNode expression) {
+
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.shiftExpression(),
+                "RelationalExpression",
+                value,
+                expression,
+                this::visitShiftExpression
+        );
+    }
+    private void visitShiftExpression(CPP14Parser.ShiftExpressionContext ctx, ExpressionNode expression) {
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.additiveExpression(),
+                "ShiftExpression",
+                value,
+                expression,
+                this::visitAdditiveExpression
+        );
+    }
+
+    private void visitAdditiveExpression(CPP14Parser.AdditiveExpressionContext ctx, ExpressionNode expression) {
+        System.out.println("Visiting AdditiveExpression");
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.multiplicativeExpression(),
+                "AdditiveExpression",
+                value,
+                expression,
+                this::visitMultiplicativeExpression
+        );
+    }
+
+    private void visitMultiplicativeExpression(CPP14Parser.MultiplicativeExpressionContext ctx, ExpressionNode expression) {
+
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.pointerMemberExpression(),
+                "MultiplicativeExpression",
+                value,
+                expression,
+                this::visitPointerMemberExpression
+        );
+    }
+    private void visitPointerMemberExpression(CPP14Parser.PointerMemberExpressionContext ctx, ExpressionNode expression) {
+
+        String value = ctx.getText();
+        visitExpression_template(
+                ctx.castExpression(),
+                "PointerMemberExpression",
+                value,
+                expression,
+                this::visitCastExpression
+        );
+    }
+    private void visitCastExpression(CPP14Parser.CastExpressionContext ctx, ExpressionNode expression) {
+        if(ctx.unaryExpression() != null){
+            visitUnaryExpression(ctx.unaryExpression(), expression);
+        }
+    }
+    private void visitUnaryExpression(CPP14Parser.UnaryExpressionContext ctx, ExpressionNode expression) {
+        if(ctx.postfixExpression() != null){
+            visitPostfixExpression(ctx.postfixExpression(),expression);
+        }
+    }
+
+    private void visitPostfixExpression(CPP14Parser.PostfixExpressionContext ctx, ExpressionNode expression) {
+
+        if(ctx.postfixExpression() != null){
+            expression.setType("PostfixExpression");
+            expression.setValue(ctx.postfixExpression().getText());
+
+            if(ctx.expressionList() != null){
+                visitPrimaryExpression(ctx.primaryExpression(), expression);
+            }
+        }
+        if (ctx.primaryExpression() != null){
+            visitPrimaryExpression(ctx.primaryExpression(), expression);
+        }
+    }
+    private void visitPrimaryExpression(CPP14Parser.PrimaryExpressionContext ctx, ExpressionNode expression) {
+
+        if(ctx.literal() != null){
+            var l = new LiteralNode();
+            l.setValue(ctx.getText());
+
+            expression.addChild(l);
+        }
     }
 }
---
+++ src/main/java/ast/ExpressionNode.java
@@ -0,0 +1,49 @@
+package ast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionNode extends ASTNode {
+
+    private String type;
+    private String operator;
+    private List<ASTNode> children = new ArrayList<ASTNode>();
+    private String value;
+
+
+    public ExpressionNode(){}
+    public ExpressionNode(List<ASTNode> children, String type){
+        this.children = children;
+        this.type = type;
+    }
+
+
+    public List<ASTNode> getChildren() { return children; }
+    public String getType() { return type; }
+    public String getOperator() {return operator; }
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+    public void setType(String type) { this.type = type; }
+    public void setOperator(String operator) { this.operator = operator; }
+    public void setChildren(List<ASTNode> children) { this.children = children; }
+
+    public void addChild(ASTNode child) { children.add(child); }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if(type != null) sb.append(type);
+        if(operator != null){
+            sb.append("[ operator: ").append(operator).append(" ]");
+        }
+        if(value != null){
+            sb.append(" [value: ").append(value).append("]");
+        }
+        if(!children.isEmpty()){
+
+            sb.append(" children: ").append(children).append("]");
+        }
+
+        return sb.toString();
+    }
+
+}
