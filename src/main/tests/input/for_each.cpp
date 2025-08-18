5f6c3e2bfa6e78da9792bd8339c97ae631a12f11
misikori <obrenovicmilos01@gmail.com>
2025-08-18 02:40:54 +0200

    [update] iteration examples

--- src/main/tests/input/for_each.cpp
+++ src/main/tests/input/for_each.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 int main() {
     int arr[] = {10, 20, 30};
-    for (int i : arr)
+    for (int i : arr){
         std::cout << i << std::endl;
+    }
     return 0;
 }
--- src/main/tests/input/for_loop_basic.cpp
+++ src/main/tests/input/for_loop_basic.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 int main() {
+    int x = -3;
     for (int i = 0; i < 3; ++i) {
+        x += i;
         std::cout << i << std::endl;
     }
-    return 0;
+    return x;
 }
